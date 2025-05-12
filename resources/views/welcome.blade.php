f6c0eb1a33b44bfb547883f15b6ae53b61abc3bc
Diego Diaz Gonzalez Manjarrez <diazdgm946@gmail>
2025-05-11 21:48:09 -0600

    Mejorar diseño de página de bienvenida y dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,29 +16,31 @@
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @endif
     </head>
-    <body>
-        <header>
+    <body class="welcome-page">
+        <header class="welcome-header">
             @if (Route::has('login'))
-                <nav>
+                <nav class="welcome-nav">
                     @auth
-                        <a href="{{ url('/dashboard') }}">Dashboard</a>
+                        <a href="{{ url('/dashboard') }}" class="nav-link">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}">Log in</a>
+                        <a href="{{ route('login') }}" class="nav-link">Log in</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}" class="nav-link">Register</a>
                         @endif
                     @endauth
                 </nav>
             @endif
         </header>
-        <main>
-            <h1>Let's get started</h1>
-            <p>Laravel has an incredibly rich ecosystem. We suggest starting with the following.</p>
-            <ul>
-                <li><a href="https://laravel.com/docs">Documentation</a></li>
-                <li><a href="https://laracasts.com">Laracasts</a></li>
-            </ul>
-        </main>
+        <div class="welcome-content-wrapper">
+            <main class="welcome-main">
+                <h1>Let's get started</h1>
+                <p>Laravel has an incredibly rich ecosystem. We suggest starting with the following.</p>
+                <ul class="welcome-links">
+                    <li><a href="https://laravel.com/docs" target="_blank" rel="noopener noreferrer">Documentation</a></li>
+                    <li><a href="https://laracasts.com" target="_blank" rel="noopener noreferrer">Laracasts</a></li>
+                </ul>
+            </main>
+        </div>
     </body>
 </html>
