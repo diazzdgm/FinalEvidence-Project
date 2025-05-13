c08b47e7228ac09aa425853eb4fe63c8ab2c787b
Diego Diaz Gonzalez Manjarrez <diazdgm946@gmail>
2025-05-12 19:48:37 -0600

    evidence

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,8 +29,9 @@
                         <div class="flex">
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
-                                <a href="{{ route('dashboard') }}" class="nav-link">
-                                    Dashboard
+                                <!-- Removed redundant Dashboard link from logo area -->
+                                <a href="{{ route('dashboard') }}">
+                                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                                 </a>
                             </div>
 
